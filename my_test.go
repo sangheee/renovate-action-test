361c6a0555b322fafe0f16fb4856fc11b24ab021
sangheee <afk.sh94@gmail.com>
2022-11-14 22:14:30 +0900

    Update my_test.go

--- my_test.go
+++ my_test.go
@@ -1,14 +1,29 @@
 package main
 
+var somePackageImageVersion = "v0.2.0" // update: depName=your.registry.address/pkgName
 
-var testPod2 = `
-apiVersion: v1
-kind: Pod
+var someK8sDeployTemplate= `
+apiVersion: apps/v1
+kind: Deployment
 metadata:
-  name: etcd
+  labels:
+    app: app
+  name: app
 spec:
-  containers:
-  - name: etcd
-    image: bitnami/etcd:3.4.20
-    ports:
-    - containerPort: 80`
+  replicas: 1
+  selector:
+    matchLabels:
+      app: app
+  template:
+    metadata:
+      labels:
+        app: app
+    spec:
+      containers:
+        - image: your.registry.address/pkgName:tag`
+
+var someConstruct = &struct{
+      version  string
+    }{
+			version:  "v0.2.0", // update: datasource=github-release depName=foo/bar
+		}
