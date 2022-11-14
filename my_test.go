b4f329feee71671ad98906aac3f8074fb6f76186
sangheee <afk.sh94@gmail.com>
2022-11-14 20:17:18 +0900

    Create my_test.go

---
+++ my_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testPod2 = `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: nginx
+spec:
+  containers:
+  - name: nginx
+    image: nginx:1.14.2
+    ports:
+    - containerPort: 80`
